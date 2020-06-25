79fd38be701354ee864bd767131ee0fd9994fbd8
pawan kumar guggilam <pawankumarguggilam@gmail.com>
2020-06-25 16:26:29 +0530

    initial commit

---
+++ src/main/java/com/demo/Application.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("Application.main()");
+	}
+}
