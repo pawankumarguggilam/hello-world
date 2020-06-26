e3f6b4bcdd0687b52d56ae76859a85d918b082f6
pawan kumar guggilam <pawankumarguggilam@gmail.com>
2020-06-26 09:15:36 +0530

    modified main application class

--- src/main/java/com/demo/Application.java
+++ src/main/java/com/demo/Application.java
@@ -1,11 +1,12 @@
 package com.demo;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("Application.main()");
+		SpringApplication.run(Application.class, args);
 	}
 }
